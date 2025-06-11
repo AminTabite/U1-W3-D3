9707a6943c00c068e40432cadb4bfe9301c64901
AminTabite <Aminepicode13@gmail.com>
2025-06-11 16:25:50 +0200

    q

--- d3w3.js
+++ d3w3.js
@@ -4,6 +4,35 @@ MyForm.addEventListener ('submit', (e) => {prevent.Default()
 const cosedafareinput = document.getElementById("cosedafare")
 
 
-const valoreCdf = cosedafareinput.value},
+const valoreCdf = cosedafareinput.value}, 
 
 console.log(`Elenco di` + valoreCdf))
+
+
+const elementi = document.createElement('li')
+
+  elementi.classList.add('elementili')
+
+  elementi.innerHTML =
+    ´<p>${valoreCdf.cosedafareinput}<p>´
+
+
+    
+    <button onclick ="deleteElementi(event)"> ELIMINA </button>
+    
+
+
+
+  const lista = document.getElementById('saved-contacts')
+  lista.appendChild(elementi)
+
+  
+  MyForm.reset()
+
+const deleteElementi = function (e) {
+  console.log('ELIMINA', e.target)
+
+
+  console.log(e.target.elementi) 
+  
+  e.target.parentElement.remove()} 
