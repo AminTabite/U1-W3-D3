6ff969aca93bd568fb389185e7b6dd66ad3654b2
AminTabite <Aminepicode13@gmail.com>
2025-06-11 16:05:04 +0200

    q

---
+++ d3w3.js
@@ -0,0 +1,9 @@
+const MyForm = document.getElementById("formTodo")
+MyForm.addEventListener ('submit', (e) => {prevent.Default()
+
+const cosedafareinput = document.getElementById("cosedafare")
+
+
+const valoreCdf = cosedafareinput.value},
+
+console.log(`Elenco di` + valoreCdf))
